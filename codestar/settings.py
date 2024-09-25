2f2a607809cd4e1b6b6b40b6df9e5be7c5cd156c
fenasti zxc <td9z0vyv@students.codeinstitute.net>
2024-09-25 14:54:13 +0000

    c

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ '8000-fenasti-djangoblog-rvbxnjlurvj.ws-eu116.gitpod.io',
     '8000-fenasti-djangoblog-ycilopv5nvl.ws-eu116.gitpod.io',
